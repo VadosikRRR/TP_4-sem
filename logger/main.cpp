ce183c2bbc83342a00ff15e98c03f997cebb776f
VadosikRRR <rechkin.77@yandex.ru>
2025-09-14 16:09:49 +0700

    Upgrade logger

--- logger/main.cpp
+++ logger/main.cpp
@@ -3,28 +3,39 @@
 #include "src/include/logger2.hpp"
 #include "src/include/logger3.hpp"
 #include "src/include/logger4.hpp"
-#include <cstdlib>
+// #include <cstdlib>
 #include <thread>
 #include <list>
 #include <chrono>
+#include <format>
 
-// std::shared_ptr<Logger1> LOGGER = Logger1::Instance();
-std::shared_ptr<Logger2> LOGGER = Logger2::Instance();
+
+std::shared_ptr<Logger1> LOGGER = Logger1::Instance();
+// std::shared_ptr<Logger2> LOGGER = Logger2::Instance();
 // std::shared_ptr<Logger3> LOGGER = Logger3::Instance();
 // std::shared_ptr<Logger4> LOGGER = Logger4::Instance();
 
 void task(double max_period, int max_iteration, int number) {
     // in ms
-    double period = max_period * rand() / RAND_MAX;
+    double period = max_period * 1000 * rand() / RAND_MAX;
 
     for (size_t i = 0; i < max_iteration; i++) {
-        std::this_thread::sleep_for(std::chrono::seconds());
+        auto start = std::chrono::system_clock::now();
+        std::string start_time = std::format("{:%H:%M:%S}", start);
+        std::this_thread::sleep_for(std::chrono::duration<double, std::milli>(period));
+        auto end = std::chrono::system_clock::now();
+        std::string end_time = std::format("{:%H:%M:%S}", end);
+
         Data data {
             std::string("This function number is ") +
             std::to_string(number) + 
-            std::string(". My period is") +
-            std::to_string(period) +
-            std::string(". That's all)\n")
+            std::string(". My period is ") +
+            std::to_string(period / 1000) +
+            std::string(". I started at ") +
+            start_time + 
+            std::string(". Finished - ") + 
+            end_time +
+            std::string(".\n")
         };
 
         LOGGER->Add(data);
@@ -34,7 +45,7 @@ void task(double max_period, int max_iteration, int number) {
 int main() {
     LOGGER->Run();
 
-    double max_period = 0.5;
+    double max_period = 0.1;
     int thread_number = 12;
     int max_iteration = 25;
     std::list<std::thread> threads;
